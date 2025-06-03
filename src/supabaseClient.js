372dc8aa6bc73127aa39f08d76b77978ae76272d
roshen.work <roshen.work@gmail.com>
2025-06-03 17:32:44 +0900

    env 경로수정

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY
+  import.meta.env.VITE_SUPABASE_ANON_KEY   // ✅ 여기 수정
 )
 
 export { supabase }
